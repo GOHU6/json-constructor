e114d20e2b967246805151fdee288a16334a1ea9
GOHU6 <oguhom@outlook.fr>
2024-11-26 22:20:26 +0100

    name type and value

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-function newobject() {
+const objectName = document.getElementById('object-name');
+const objectType = document.getElementById('object-type');
+const objectValue = document.getElementById('object-value');
+
+function toggleInputType() {
+    if (!objectName.value.trim()) {
+        objectType.disabled = true;
+        objectType.value = "";
+        objectValue.disabled = true;
+        objectValue.value = "";
+    } else {
+        objectType.disabled = false;
+    }
+}
+
+function toggleInputValue() {
+    if (!objectType.value.trim()) {
+        objectValue.disabled = true;
+        objectValue.value = "";
+    } else {
+        objectValue.disabled = false;
+    }
+}
+
+objectName.addEventListener('input', toggleInputType);
+objectType.addEventListener('input', toggleInputValue);
+toggleInputType();
+toggleInputValue();
+
+
+function createobject() {
     console.log('Start...');
+
+    const objectName = document.getElementById('object-name').value.trim();
+    
+    if (!objectName) {
+        document.getElementById('object-value').disabled = false;
+    }
+    
+    const objectValue = document.getElementById('object-value').value.trim();
+    
+   
+
     const newobject = document.getElementById('contentID');
     
+
+
     let jsonObject = {
-        valeur1: "Json Constructor",
-        valeur2: "Voici un objet JSON généré dynamiquement !",
-        valeur3: new Date().toLocaleString()
+        [objectName]: objectValue, 
     };
 
-    jsonObject.newContent = "Nouveau contenu ajouté";
 
     newobject.textContent = JSON.stringify(jsonObject, null, 2); 
     
